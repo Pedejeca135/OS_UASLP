49d6e21585e18120017a0de8bfcc5935805db825
pedejeca135 <pjco90@hotmail.com>
2020-03-09 11:36:47 -0600

    falta probarlo con make

---
+++ NachOS/nachos/test/prueba_0.c
@@ -0,0 +1,22 @@
+/* prueba_0.c 
+	Agregada: 25/Feb/2020.
+ *
+ */
+
+#include "syscall.h"
+
+int
+main()
+{
+	int arrTam = 200;
+	int arreglo[arrTam];	
+	
+	int i;
+	for(i = 0 ; i<arrTam; i++)
+	{
+		arreglo[i] = 1;
+	}
+{
+}
+    Halt();		/* and then we're done */
+}
